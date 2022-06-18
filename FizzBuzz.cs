667243ef3855d213ae509254f5c88268742eec7a
Daniel Pokryshkin <74131860+dstish@users.noreply.github.com>
2022-06-18 22:04:14 +0300

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        static void Main()
+        {
+            DoFizzBuzz();
+        }
+
+        static void DoFizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string output = "";
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+                if (output.Length == 0)
+                {
+                    output = i.ToString();
+                }
+                Console.WriteLine(output);
+
+            }
+        }
+    }
+}
